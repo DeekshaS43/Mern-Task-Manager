85fdb5cc7af22f9e16fd412caede0f6ea0bf8677
DeekshaS43 <deekshasachan9343@gmail.com>
2024-06-22 16:18:49 +0530

    backend

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mern-task-manager-51l6.onrender.com/api",
+  baseURL: "https://mern-task-manager-1-bkem.onrender.com/api",
 });
 export default api;
